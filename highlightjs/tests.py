8ece95a72371c1b77c2bb2b7696181933c92f430
Mounir <messelmeni.mounir@gmail.com>
2024-07-04 22:37:39 +0200

    Update packages (#144)
    * Update packages
    Switch formatting to Ruff
    
    * Update when to run pre-commit

--- highlightjs/tests.py
+++ highlightjs/tests.py
@@ -8,31 +8,23 @@ from . import settings
 
 class HighlightjsTemplateTagTests(TestCase):
     def test_highlightjs_css_url_tag(self):
-        out = Template("{% load highlightjs %}{% highlightjs_css_url %}").render(
-            Context()
-        )
+        out = Template("{% load highlightjs %}{% highlightjs_css_url %}").render(Context())
         self.assertEqual(out, settings.css_url())
 
     def test_highlightjs_javascript_tag(self):
-        out = Template("{% load highlightjs %}{% highlightjs_javascript %}").render(
-            Context()
-        )
+        out = Template("{% load highlightjs %}{% highlightjs_javascript %}").render(Context())
         self.assertIn(settings.highlightjs_url(), out)
 
     @override_settings(HIGHLIGHTJS={"include_jquery": False})
     def test_highlightjs_javascript_tag_without_jquery(self):
         settings.update_settings()
-        out = Template("{% load highlightjs %}{% highlightjs_javascript %}").render(
-            Context()
-        )
+        out = Template("{% load highlightjs %}{% highlightjs_javascript %}").render(Context())
         self.assertNotIn(settings.highlightjs_jquery_url(), out)
 
     @override_settings(HIGHLIGHTJS={"include_jquery": True})
     def test_highlightjs_javascript_tag_with_jquery(self):
         settings.update_settings()
-        out = Template("{% load highlightjs %}{% highlightjs_javascript %}").render(
-            Context()
-        )
+        out = Template("{% load highlightjs %}{% highlightjs_javascript %}").render(Context())
         self.assertIn(settings.highlightjs_jquery_url(), out)
 
     def test_highlightjs_this_tag(self):
@@ -41,8 +33,6 @@ class HighlightjsTemplateTagTests(TestCase):
             "for i, name in enumerate(friends):"
             "    print 'iteration {iteration} is {name}'.format(iteration=i, name=name)"
         )
-        out = Template("{% load highlightjs %}{% highlightjs_this code %}").render(
-            Context({"code": code})
-        )
+        out = Template("{% load highlightjs %}{% highlightjs_this code %}").render(Context({"code": code}))
         self.assertIn(escape(code), out)
         self.assertIn("<pre>", out)
